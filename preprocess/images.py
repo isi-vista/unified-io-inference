f510df4a9f030de92bcab4722b8b93f7803d0683
yash-reddy <write2yashreddy@gmail.com>
2023-03-28 02:00:15 -0400

    Add support for sampling datasets

--- preprocess/images.py
+++ preprocess/images.py
@@ -1,16 +1,26 @@
 from argparse import ArgumentParser
 from pathlib import Path
+import random
 from uio.runner import IMAGE_TAGGING
+
+random.seed(0)
 
 DEFAULT_QUESTION = IMAGE_TAGGING
 
-def preprocess_images(images_dir: Path):
+def preprocess_images(images_dir: Path, n_samples = -1):
 	uio_file_contents = []
 	img_filetypes = ['*.jpg','*.png']
+	filepaths = []
 	for filetype in img_filetypes:
-			for input_file in images_dir.rglob(filetype):
-				uio_line = f"{input_file.absolute()}:{DEFAULT_QUESTION}"
-				uio_file_contents.append(uio_line)
+		filepaths.extend(sorted(images_dir.rglob(filetype)))
+
+	if n_samples > 0:
+		filepaths = random.sample(filepaths, n_samples)
+
+	for input_file in filepaths:
+		uio_line = f"{input_file.absolute()}:{DEFAULT_QUESTION}"
+		uio_file_contents.append(uio_line)
+
 	return uio_file_contents 
 
 def main():
@@ -26,9 +36,11 @@ def main():
 			help="Output file to write the unified inputs.",
 	)
 
+	parser.add_argument("--n-samples", type=int, help="Number of samples to use. All images are used if this argument is not specified.")
+
 	args = parser.parse_args()
-	
-	uio_file_contents = preprocess_images(args.input_dir)
+
+	uio_file_contents = preprocess_images(args.input_dir, n_samples= args.n_samples)
 	with args.output_file.open('w') as fp:
 		fp.write('\n'.join(uio_file_contents))
 
