a414212877c718f5d746a627631dc82c58f1a407
yash-reddy <write2yashreddy@gmail.com>
2023-03-28 02:29:16 -0400

    Remove borders before processing objectnet images

--- preprocess/images.py
+++ preprocess/images.py
@@ -2,16 +2,18 @@ from argparse import ArgumentParser
 from pathlib import Path
 import random
 from uio.runner import IMAGE_TAGGING
+from PIL import Image
+from numpy import asarray
 
 random.seed(0)
 
 DEFAULT_QUESTION = IMAGE_TAGGING
+IMG_FILETYPES = ['*.jpg','*.png']
 
 def preprocess_images(images_dir: Path, n_samples = -1):
 	uio_file_contents = []
-	img_filetypes = ['*.jpg','*.png']
 	filepaths = []
-	for filetype in img_filetypes:
+	for filetype in IMG_FILETYPES:
 		filepaths.extend(sorted(images_dir.rglob(filetype)))
 
 	if n_samples > 0:
@@ -36,11 +38,35 @@ def main():
 			help="Output file to write the unified inputs.",
 	)
 
+	parser.add_argument(
+		"--objectnet-images",
+		action = "store-true",
+		help="Flag to indicate that the provided images belong to the Objectnet Dataset. Thee 1px border is removed and the resultant images are stored in a different directory if set to True."
+	)
+
+	parser.add_argument(
+			"--objectnet-output", type=Path, help="Output directory to store processed objectnet images."
+	)
+
 	parser.add_argument("--n-samples", type=int, help="Number of samples to use. All images are used if this argument is not specified.")
 
 	args = parser.parse_args()
+	input_dir = args.input_dir
 
-	uio_file_contents = preprocess_images(args.input_dir, n_samples= args.n_samples)
+	if args.objectnet_images:
+		for filetype in IMG_FILETYPES:
+			for input_file in sorted(args.input_dir.rglob(filetype)):
+				image = Image.open(input_file)
+				data = asarray(image)
+				borderless_data = data[1:-1, 1:-1]
+				borderless_image = Image.fromarray(borderless_data)
+				
+				filename = input_file.name
+				borderless_image.save(args.objectnet_output / filename)
+
+		input_dir =  args.objectnet_output
+
+	uio_file_contents = preprocess_images(input_dir, n_samples= args.n_samples)
 	with args.output_file.open('w') as fp:
 		fp.write('\n'.join(uio_file_contents))
 
