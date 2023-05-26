91b60d84c41a8c4fc3520bb96a1ae196ecaff893
Daniel Napierski <napiersk@isi.edu>
2023-05-26 07:19:10 -0700

    clean up

--- generate_claims.py
+++ generate_claims.py
@@ -7,37 +7,9 @@ import json
 import os
 from collections import defaultdict
 from typing import List, Dict
-#from absl import logging
 from pathlib import Path
 
 def main():
-#  with open("/nas/gaia02/users/napiersk/github/may01/unified-io-inference/claims_shortened.json") as claim_file:
-#    parsed_claims = json.load(claim_file)
-#  with open("out_claims.json", 'w') as f:
-#    json.dump(parsed_claims, f, indent=2, sort_keys=True)
-#    exit()
-#  exit()
-#  with open("/nas/gaia02/users/napiersk/github/may01/unified-io-inference/single_claim.json") as claim_file:
-#    parsed_claims = json.load(claim_file)
-#    print(f"Claims keys: {parsed_claims.keys()}")
-#    print(f"first claim: {parsed_claims['claims'][0].keys()}")
-# JSON FORMAT:
-#Claims keys: dict_keys(['claims'])
-#first claim: dict_keys(
-#['sentence', 'claim_span_text', 'claimbuster_score', 'claimer_end', 'start_char', 
-#'time_end_earliest', 'claimer_debug', 'associated_KEs', 'claimer_ke', 'claim_span_end', 
-#'claim_id', 'x_ke_typeqnode', 'claim_span_start', 'equivalent_claims', 'time_end_latest', 
-#'x_end', 'claimer_start', 'end_char', 'refuting_claims', 'time_start_earliest', 'template', 
-#'final_claim_score', 'claimer_ke_typeqnode', 'x_start', 'stance', 'x_ke', 'news_author', 
-#'claim_semantics', 'topic_score', 'time_start_latest', 'claimer_ke_qnode', 'topic', 'claimer_text', 
-#'segment_id', 'x_variable', 'claimer_score', 'claimer_qnode', 'qnode_x_variable_identity', 
-#'qnode_x_variable_type', 'x_ke_qnode', 'claimer_type_qnode', 'sub_topic', 'supporting_claims', 
-#'news_url', 'source', 'claimer_affiliation', 'claimer_affiliation_identity_qnode', 
-#'claimer_affiliation_type_qnode', 'location', 'entity', 'lan', 'generation', 
-#'time_attr', 'sentence_L', 'sentence_M', 'sentence_R', 'equivalent_claims_text', 
-#'supporting_claims_text', 'refuting_claims_text', 'claimer_search_key', 'render_text']
-#)
-
   parser = argparse.ArgumentParser()
   parser.add_argument("tsv_files", help="Comma-separated list of input file paths")
   parser.add_argument("output_file", default="generated_claims.json")
